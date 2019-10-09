c6d73ff25d905ca947a11c7dd633fbc8889e6292
teemi <tm@teemii.com>
2019-10-10 00:30:36 +0800

    update fs.WorkingPath

--- util/fs/fs.go
+++ util/fs/fs.go
@@ -13,7 +13,8 @@ func WorkingPath(offset string) string {
 		wp := os.Getenv("GO_WORKING_PATH")
 
 		if wp != "" {
-			return path.Join(Dir(wp), offset)
+			wp = path.Clean(wp)
+			return path.Join(wp, offset)
 		}
 	}
 
